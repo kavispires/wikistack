e0af790456295a02b05b734c4830510325ef34a3
kavispires <kavispires@gmail.com>
2017-01-20 15:06:49 -0800

    Initial Files and modules

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var nunjucks = require('nunjucks');
+var models = require('./models');
+
+app.use(express.static('public'));
+
+// point nunjucks to the directory containing templates and turn off caching; configure returns an Environment 
+// instance, which we'll want to use to add Markdown support later.
+var env = nunjucks.configure('views', {noCache: true});
+// have res.render work with html files
+app.set('view engine', 'html');
+// when res.render works with html files, have it use nunjucks to do so
+app.engine('html', nunjucks.render);
+
+app.get('/', (req,res,next) => {
+	res.render('index');
+})
+
+models.User.sync({})
+.then(function () {
+    return models.Page.sync({})
+})
+.then(function () {
+    app.listen(3000, function () {
+        console.log('Server is listening on port 3000!');
+    });
+})
+.catch(console.error);
